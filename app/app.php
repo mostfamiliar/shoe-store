042e4b7c622ffcb61f45a76b7a6319234728cc83
torrence stratton <student@epicodus.com>
2016-03-04 10:07:38 -0800

    add delete all to stores

--- app/app.php
+++ app/app.php
@@ -17,8 +17,20 @@
     ));
 
     $app->get("/", function() use ($app){
-
       return $app['twig']->render('index.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post("/add_store", function() use ($app){
+        $store_name = $_POST['name'];
+        $id = null;
+        $new_store = new Store($id, $store_name);
+        $new_store->save();
+      return $app['twig']->render('index.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post("/delete_all", function() use ($app){
+        Store::deleteAll();
+     return $app['twig']->render('index.html.twig', array('stores' => Store::getAll()));
     });
 
     return $app;
--- src/Brand.php
+++ src/Brand.php
@@ -1,4 +1,4 @@
-<<?php
+<?php
     class Brand {
 
         private $id;
